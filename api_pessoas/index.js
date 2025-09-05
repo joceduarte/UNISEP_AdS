8ca0d6f1ec3143f3f9217c5f6f15ad9476c7c3b4
Jocemar <jocemarduarte4@gmail.com>
2025-09-04 22:15:25 -0300

    Atualização,put,delete

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -18,10 +18,38 @@ app.post("/cadastro", (request, response) => {
     pessoas.push({
         id: countID++,
         nome,
-        telefone    
+        telefone
     });
 
     return response.send({ nome, telefone });
+});
+
+app.put("/atualizar/:id", (request, response)=>{
+    const { id } = request.params;
+    const { nome, telefone } = request.body;
+
+    const pessoaIndex = pessoas.findIndex((i) => i.id == id);
+
+    if(pessoaIndex <= -1){
+        return response.send({ msg: "O codigo a pessoa não existe!"});
+    }
+
+    pessoas[pessoaIndex].nome = nome;
+    pessoas[pessoaIndex].telefone = telefone;
+
+    return response.send({ nome, telefone});
+});
+
+app.delete("/deletar/:id", (request, response)=>{
+    const { id } = request.params;
+
+    const pessoaIndex = pessoas.findIndex((i) => i.id == id);
+    if(pessoaIndex <= -1){
+        return response.send({msg: "O código a pessoa esta invalido"});
+    }else{
+        pessoas.splice(pessoaIndex,1);
+    }
+    return response.send({msg: "Apagado com sucesso"});
 });
 
 app.listen(8080, () => {
